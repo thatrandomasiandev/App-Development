dc66d2267b6867eb67819b31ed14777892965c11
thatrandomasiandev <terranovajoshua1@gmail.com>
2025-06-28 16:41:08 -0700

    Delete PasteX^3 directory

--- PasteX^3/KeyboardShortcuts+Extensions.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import KeyboardShortcuts
-
-extension KeyboardShortcuts.Name {
-    static let togglePasteX = Self("togglePasteX", default: .init(.v, modifiers: [.command, .shift]))
-} 
