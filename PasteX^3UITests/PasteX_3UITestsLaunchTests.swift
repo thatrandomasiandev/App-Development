afea07b88ac2f1f2c7a9e4c4a82e66b2acac484c
thatrandomasiandev <terranovajoshua1@gmail.com>
2025-06-28 16:46:35 -0700

    Add files via upload

---
+++ PasteX^3UITests/PasteX_3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PasteX_3UITestsLaunchTests.swift
+//  PasteX^3UITests
+//
+//  Created by Joshua Terranova on 6/28/25.
+//
+
+import XCTest
+
+final class PasteX_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
